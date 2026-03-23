279c2ebb805f044665d117c777e7ad73f3d31bd5
BlaketripB <Buddy.butler105@gmail.com>
2026-03-22 20:00:38 -0600

    Delete .claude directory

--- .claude/serve.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import os
-
-os.chdir("/Users/blakebutler/Library/Mobile Documents/com~apple~CloudDocs/dr-wright-website")
-
-handler = http.server.SimpleHTTPRequestHandler
-httpd = http.server.HTTPServer(("", 3000), handler)
-print("Serving at http://localhost:3000")
-httpd.serve_forever()
